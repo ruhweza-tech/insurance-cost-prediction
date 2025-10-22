23a46cb9efeb156c29ef4afddcfb08c5748e959d
Ruhweza Mugisha <mugisha.ruhweza@fortum.com>
2025-10-22 23:05:19 +0200

    Modified the clean_data file

--- insurance_cost_prediction/scripts/clean_data.py
+++ insurance_cost_prediction/scripts/clean_data.py
@@ -3,45 +3,48 @@
 import pandas as pd 
 import numpy as np 
 import os 
+import joblib
 
 
-# Define the path of the raw dataset 
+# Define the path for the raw dataset 
+
 raw_path = r"C:\Users\ruhwemug\ML_Projects\my_project\insurance_cost_prediction\data\raw\insurance.csv"
 
-# Create a function to clean the data 
+# Create a function that will clean the raw dataset
 def clean_data(raw_path):
 
     # Load dataset 
     dataset = pd.read_csv(raw_path)
 
-    # Encode binary columns 
-    dataset["sex"] = dataset["sex"].apply(lambda m : 0 if m == "female" else 1)
+    # Encode the binary columns 
+    dataset["sex"] = dataset["sex"].apply(lambda m: 0 if m == "female" else 1)
     dataset["smoker"] = dataset["smoker"].apply(lambda m: 0 if m == "no" else 1)
 
-    # One - hot encode region column 
-    region_dumies = pd.get_dummies(dataset["region"], drop_first = True, dtype = int)
-    dataset = pd.concat([region_dumies, dataset], axis = 1)
+    # One hot encode region
+    region_dummies = pd.get_dummies(dataset["region"], drop_first = True, dtype = int)
+    dataset = pd.concat([region_dummies, dataset], axis = 1)
     dataset.drop(["region"], axis = 1, inplace = True)
-    
+
     return dataset
 
+
+# create a single entry point
 if __name__ == "__main__":
 
-    # Define the folder where you want to store the cleaned dataset
+    # Define the folder path to store the cleaned datatset 
     folder = r"C:\Users\ruhwemug\ML_Projects\my_project\insurance_cost_prediction\data\clean"
 
-    # Check if the folder exists,  if its missing create one 
-    os.makedirs(folder, exist_ok = True)
+    # Check if the folder exists , if missing create one
+    os.makedirs(folder, exist_ok = True )
 
-    # Create a full path in this folder to save the cleaned dataset
-    clean_path = os.path.join(folder, "insurance_cleaned.csv")
+    # Create a full path in the folder to save the cleaned dataset
+    save_path = os.path.join(folder , "insurance_cleaned.csv")
 
-    # call the functiuon to the clean the dataset 
-    data_cleaned = clean_data(raw_path)
+    # Call the function the clean the dataset 
+    clean_dataset = clean_data(raw_path)
 
-    # Store the cleaned dataset to the full path created 
-    data_cleaned.to_csv(clean_path, index = False)
-    print(data_cleaned.head())
+    # Save the cleaned dataset in the path
+    clean_dataset.to_csv(save_path, index = False)
 
-
-
+    print(clean_dataset.head())
+    print(f"Cleaned dataset saved at: {save_path}")
