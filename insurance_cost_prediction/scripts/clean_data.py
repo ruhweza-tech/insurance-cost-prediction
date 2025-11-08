4925e3a36a5d5189ccbea3c752c810a68a8d8439
ruhweza-tech <mugishamacbeth@hotmail.com>
2025-11-08 23:18:12 +0100

    updated the clead data script

--- insurance_cost_prediction/scripts/clean_data.py
+++ insurance_cost_prediction/scripts/clean_data.py
@@ -1,75 +1,67 @@
 
-# Import required libraries 
-import os 
-import json 
+# Import the required libraries
 import pandas as pd 
+import json 
 from pathlib import Path 
-from dotenv import load_dotenv
+import numpy as np 
 
-# Load the .env variables and define the base working directory 
-load_dotenv()
+# Define the working base directory [2 levels up]
 BASE_DIR = Path(__file__).resolve().parents[1]
 
-# Define the path to config and load the configuration variables from json
+# Define the path to the config and load the variables from json
 CONFIG_PATH = BASE_DIR/"config"/"insurance_config.json"
 with open(CONFIG_PATH) as m:
     config = json.load(m)
 
-# Define the path to the raw dataset and the path to the folder that will store the cleaned dataset
+# Define the path to the raw dataset and the folder to store the cleaned dataset
 RAW_PATH = BASE_DIR/config["raw_path"]
-CLEAN_FOLDER = BASE_DIR/config["clean_data_folder"]
+CLEAN_PATH = BASE_DIR/config["clean_data_folder"]
 
-# Check if the clean folder exists , if missing create one 
-CLEAN_FOLDER.mkdir(parents = True, exist_ok = True)
+# Check if the folder to store the clean dataset exists, if missing , automatically create one
+CLEAN_PATH.mkdir(parents = True, exist_ok = True)
 
-# Create a full path in the clean folder to store the cleaned dataset
-SAVE_PATH = CLEAN_FOLDER/"insurance_cleaned.csv"
+# Create a full path in the folder to store the cleaned dataset
+PROCESSED = CLEAN_PATH/"insurance_cleaned.csv"
 
-def clean_data(raw_data: Path):
-    """
+def clean_data(raw_path: Path):
+    """ 
     Function to clean the raw dataset
         - Load the raw dataset
-        - Encode the binary columns 
-        - One-hot encoding 
-        - Return the clean_dataset
+        - Encode binary columns 
+        - One-hot encode 
+        - Return the output
     """
-    # Load the raw dataset 
-    clean_dataset = pd.read_csv(raw_data)
+    # Load the raw dataset
+    raw_dataset = pd.read_csv(raw_path)
 
-    # Encode the binary categorical variables
-    clean_dataset["sex"] = clean_dataset["sex"].apply(lambda m: 0 if m == "female" else 1)
-    clean_dataset["smoker"] = clean_dataset["smoker"].apply(lambda m: 0 if m == "no" else 1)
+    # Encode binary columns 
+    raw_dataset["sex"] = raw_dataset["sex"].apply(lambda m: 0 if m == "female" else 1)
+    raw_dataset["smoker"] = raw_dataset["smoker"].apply(lambda m: 0 if m == "no" else 1)
 
-    # One-hot encoding the region column and droping the first category to prevent dummy trap
-    region_dummies = pd.get_dummies(clean_dataset["region"], drop_first = True, dtype = int)
-    clean_dataset = pd.concat([region_dummies, clean_dataset], axis = 1)
-    clean_dataset.drop(["region"], axis = 1, inplace = True)
+    # One-hot encode region column
+    region_dummies = pd.get_dummies(raw_dataset["region"], drop_first = True, dtype = int)
+    raw_dataset = pd.concat([region_dummies, raw_dataset], axis = 1)
+    raw_dataset.drop(["region"], axis = 1, inplace = True)
 
-    return clean_dataset
+    # Retrun the output
+    return raw_dataset
+
 
 if __name__ == "__main__":
     """
-    Single entry point as the main execution block
-        - Call the function to clean the raw dataset
-        - Save the cleaned dataset to the full path already created earlier on
-        - Display a message indicating where the cleaned dataset is saved
+    Single entry point to act is the mian execution block
+        - Call the fucntion to clean the raw dataset
+        - Store the cleaned dataset in the folder
+        - Display the output
     """
-    # Call the fucntion to clean the raw dataset
-    clean_dataset = clean_data(RAW_PATH)
+    # Call the fucntion to clean the dataset
+    processed_data = clean_data(RAW_PATH)
 
-    # Save the cleaned dataset to the clean folder path
-    clean_dataset.to_csv(SAVE_PATH, index = False)
+    # Store the cleaned dataset in the folder
+    processed_data.to_csv(PROCESSED, index = False)
 
-    # Display the output
-
-    print("\n====================================================================================")
-    print(clean_dataset.head())
+    print("\n=================================================================================")
+    print(processed_data.sample(5))
     print(" ")
-    print(f"The cleaned dataset is saved at: {SAVE_PATH}")
-    print("====================================================================================\n")
-
-    print("\n====================================================================================")
-    print(f"Environment is : {os.getenv("ENVIRONMENT", "local")}")
-    print(f"The author for this script is called: {os.getenv("AUTHOR", "Mugisha")}")
-    print("====================================================================================\n")
-    
+    print(f"The cleaned dataset is saved at: {PROCESSED}")
+    print("=================================================================================\n")
